d3d0e03eda99458244bdd02445450fa7214fba36
gisfun <gisfun>
2025-09-19 12:41:23 +0800

    WSL2: mount host daemon into container

--- test/start.sh
+++ test/start.sh
@@ -8,5 +8,18 @@
 source /app/bashrc
 cp /app/bashrc /home/arcgis/.bashrc
 
+sudo chgrp docker /var/run/docker.sock
 
-tail -f /var/log/*.log
+SCRIPT="/home/arcgis/notebookserver/startnotebookserver.sh"
+if [ -f $SCRIPT ]; then
+  # Starting ArcGIS Server
+  $SCRIPT
+else
+  echo "Installing Notebook"
+  /app/Installers/NotebookServer_Linux/Setup -m silent -d /home --verbose -l yes -a /app/licenses/keycodes.ecp
+fi
+
+# 1050:100
+sudo chown -R arcgis:users /home/arcgis/arcgisnotebookserver/directories
+
+tail -f /var/log/*.log /home/arcgis/arcgisnotebookserver/logs/**/*.log
